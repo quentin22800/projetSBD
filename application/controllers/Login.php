993423599ab63378b19d602c910ad248969088c2
quentin22800 <24philippe>
2018-03-14 09:59:31 +0100

    debut page login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('header.html')
+		$this->load->view('header.html');
 		$this->load->view('login.php');
 		$this->load->view('footer.html');
 	}
