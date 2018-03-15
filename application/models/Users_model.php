169d462a965b61d93aeba65bf5bbe4c23229ae78
KevinBoisgontier <kevin.boisgontier26@gmail.com>
2018-03-15 16:34:43 +0100

    test

---
+++ application/models/Users_model.php
@@ -0,0 +1,29 @@
+<?php
+
+class Users_model extends CI_Model {
+
+        public function getUsers()
+        {
+                $query = $this->db->get('users');
+                return $query->result();
+        }
+
+        public function insert_entry()
+        {
+                $this->title    = $_POST['title']; // please read the below note
+                $this->content  = $_POST['content'];
+                $this->date     = time();
+
+                $this->db->insert('entries', $this);
+        }
+
+        public function update_entry()
+        {
+                $this->title    = $_POST['title'];
+                $this->content  = $_POST['content'];
+                $this->date     = time();
+
+                $this->db->update('entries', $this, array('id' => $_POST['id']));
+        }
+
+}
