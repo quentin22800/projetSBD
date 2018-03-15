f0e73927969b6dd84cecc06aa1775b424c9fa0ed
KevinBoisgontier <kevin.boisgontier26@gmail.com>
2018-03-15 17:17:52 +0100

    db connection

--- application/config/database.php
+++ application/config/database.php
@@ -76,10 +76,10 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'postgres',
-	'password' => 'postgres',
+	'username' => 'root',
+	'password' => '',
 	'database' => 'projetSBD',
-	'dbdriver' => 'postgre',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
