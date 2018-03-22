afc7a1837bd91186a4d763bcd202cb742dd9f918
quentin22800 <24philippe>
2018-03-22 17:32:37 +0100

    css

--- application/views/login.php
+++ application/views/login.php
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<!-- LOGIN FORM -->
-<div class="text-center" style="padding:50px 0">
-	<div class="logo">login</div>
-	<!-- Main Form -->
-	<div class="login-form-1">
-		<form id="login-form" class="text-left">
-			<div class="login-form-main-message"></div>
-			<div class="main-login-form">
-				<div class="login-group">
-					<div class="form-group">
-						<label for="lg_username" class="sr-only">Username</label>
-						<input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="username">
-					</div>
-					<div class="form-group">
-						<label for="lg_password" class="sr-only">Password</label>
-						<input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="password">
-					</div>
-					<div class="form-group login-group-checkbox">
-						<input type="checkbox" id="lg_remember" name="lg_remember">
-						<label for="lg_remember">remember</label>
-					</div>
-				</div>
-				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-			</div>
-			<div class="etc-login-form">
-				<p>forgot your password? <a href="#">click here</a></p>
-				<p>new user? <a href="#">create new account</a></p>
-			</div>
-		</form>
-	</div>
-	<!-- end:Main Form -->
-</div>
-=======
 <section id="login">
     <div class="container">
     	<div class="row">
@@ -59,4 +25,3 @@
     	</div> <!-- /.row -->
     </div> <!-- /.container -->
 </section>
->>>>>>> 4dd696c720dce0313a233677d6d4ba4cfce7ef8f
--- application/views/requeteView.php
+++ application/views/requeteView.php
@@ -1,16 +1,28 @@
-<div>
-	<h1>Choix de la requête</h1>
-	<form>
-		<div class="form-group">
-			<label for="exampleFormControlSelect1">Requêtes : </label>
-			<select class="form-control" id="exampleFormControlSelect1" id="requestList">
-				<?php 
-				foreach ($requests as $r) {
-					echo "<option>".$r."</option>";
-				}
-				?>
-			</select>
 
-		</div>
-	</form>
-</div>
+	<div class="container">
+	<h3>Choix de la requête</h3>
+    	<div class="row">
+			<div class="col-xs-2">
+			</div>
+    	    <div class="col-xs-8">
+        	    <div class="form-wrap">
+                	<form>
+						<div class="form-group">
+							<label for="exampleFormControlSelect1">Requêtes : </label>
+							<select class="form-control" id="exampleFormControlSelect1" id="requestList">
+								<?php 
+									foreach ($requests as $r) {
+										echo "<option>".$r."</option>";
+									}
+								?>
+							</select>
+
+						</div>
+					</form>
+        	    </div>
+			</div>
+			<div class="col-xs-2">
+			</div>
+    	</div>
+    </div>
+
